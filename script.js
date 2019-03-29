bac2f65039f8da1972a0b1461d378e18dc20599f
realVato <44781061+realVato@users.noreply.github.com>
2019-03-29 23:44:45 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,33 @@
+/* 
+ QUIZ GAME
+ --------------------
+ 1. Build a function constructor called Question 
+ to describe a question.
+ A question should include:
+ a) question itself
+ b) the answers from which the player can choose the correct one
+ (Choose and adequate data structure here, array, object, etc.)
+ c) correct answer (number 0 1)
+
+ 2. Create a couple of questions using the constructor
+
+ 3. Store them all inside an array
+
+ 4. Select one random question and log it on the console,
+ together with the possible answers
+ (each question should havea a number)
+ (Hint: Write a method for the question objects for this task).
+
+ 5. Use the 'prompt' function to ask the user for the correct 
+ answer.
+ The user should input the number of the corrent answer
+ such as displayed in task 4.
+
+ 6. Check if the answer is correct and print to the console
+ whether the answer is correct or not (Write another method).
+
+ 7. Suppose this code would be a plugin for other programmers
+ to use in their code. So make sure that all the code is private
+ and doesnt interfere with the other programmers code.
+
+*/
