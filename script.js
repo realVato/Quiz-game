d66dd33162eaa52254e38118077023169ba4ee2a
realVato <44781061+realVato@users.noreply.github.com>
2019-04-06 13:29:34 +0300

    Code is plugin like

--- script.js
+++ script.js
@@ -45,93 +45,95 @@ an answer is correct, add 1 point to the score.
 11. Display the score in the console. (Yet another method for this).
 
 */
-var score = 0;
+(function(){
+    var score = 0;
 
-function Question(question, answers, correctAnswerNO) {
-
-    this.question = question;
-    this.answers = answers;
-    this.correctAnswerNO = correctAnswerNO;
-}
-
-Question.prototype.randomElement = function () {
+    function Question(question, answers, correctAnswerNO) {
     
-    var randomQuestion = this.question[Math.floor(Math.random() * this.question.length)];
-    
-    console.log(randomQuestion);
-
-    if (randomQuestion === this.question[0]) {
-        console.log(this.answers[0][0]);
-        console.log(this.answers[0][1]);
-        this.checkAnswer();
-        
-    } else if (randomQuestion === this.question[1]) {
-        console.log(this.answers[1][0]);
-        console.log(this.answers[1][1]); 
-        this.checkAnswer();
-        
-    } else {
-        console.log(this.answers[2][0]);
-        console.log(this.answers[2][1]);
-        this.checkAnswer();
+        this.question = question;
+        this.answers = answers;
+        this.correctAnswerNO = correctAnswerNO;
     }
-}
-
-Question.prototype.checkAnswer = function() {
-        var userInput;
+    
+    Question.prototype.randomElement = function () {
         
-
-        userInput = prompt('answer');
-        if (userInput == this.correctAnswerNO[0]) {
+        var randomQuestion = this.question[Math.floor(Math.random() * this.question.length)];
+        
+        console.log(randomQuestion);
+    
+        if (randomQuestion === this.question[0]) {
+            console.log(this.answers[0][0]);
+            console.log(this.answers[0][1]);
+            this.checkAnswer();
             
-            score++;
-            console.log('correct');
-            console.log('Your current score is:' + score);
-            console.log('--------------------------------');
-            console.log('')
-            quiz.randomElement();
-
-        } else if (userInput == this.correctAnswerNO[1]) {
-            console.log('incorrect, try again');
-            console.log('Your current score is:' + score);
-            console.log('--------------------------------');
-            console.log('');
-            quiz.randomElement();
-
-        } else if (userInput == 'exit') {
-            console.log('Your final score is:' + score);
-            console.log('Game ended.');
+        } else if (randomQuestion === this.question[1]) {
+            console.log(this.answers[1][0]);
+            console.log(this.answers[1][1]); 
+            this.checkAnswer();
+            
         } else {
-            console.log('Please submit an answer');
+            console.log(this.answers[2][0]);
+            console.log(this.answers[2][1]);
+            this.checkAnswer();
         }
     }
-
-var quiz = new Question(
-
-    [   
-        'Is JS a good language?',
-        'Is JS fun?',
-        'Is earth flat?'
-    ],
-    [
-        [   
-            '0: It\'s very good!',
-            '1: There are better ones..'
-        ],
-        [   
-            '0: It\'s very fun!',
-            '1: It\'s hard!'
-        ],
-        [   
-            '0: No.',
-            '1: No.'
-        ]
-    ],
-    [
-        '0',
-        '1',
-    ]
     
-    );
-
-quiz.randomElement();
+    Question.prototype.checkAnswer = function() {
+            var userInput;
+            
+    
+            userInput = prompt('answer');
+            if (userInput == this.correctAnswerNO[0]) {
+                
+                score++;
+                console.log('correct');
+                console.log('Your current score is:' + score);
+                console.log('--------------------------------');
+                console.log('')
+                quiz.randomElement();
+    
+            } else if (userInput == this.correctAnswerNO[1]) {
+                console.log('incorrect, try again');
+                console.log('Your current score is:' + score);
+                console.log('--------------------------------');
+                console.log('');
+                quiz.randomElement();
+    
+            } else if (userInput == 'exit') {
+                console.log('Your final score is:' + score);
+                console.log('Game ended.');
+            } else {
+                console.log('Please submit an answer');
+            }
+        }
+    
+    var quiz = new Question(
+    
+        [   
+            'Is JS a good language?',
+            'Is JS fun?',
+            'Is earth flat?'
+        ],
+        [
+            [   
+                '0: It\'s very good!',
+                '1: There are better ones..'
+            ],
+            [   
+                '0: It\'s very fun!',
+                '1: It\'s hard!'
+            ],
+            [   
+                '0: No.',
+                '1: No.'
+            ]
+        ],
+        [
+            '0',
+            '1',
+        ]
+        
+        );
+    
+    quiz.randomElement();
+})();
